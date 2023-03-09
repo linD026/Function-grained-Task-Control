9a96c423a80cd3db8fb6448733a63ccea78beb47
Chih-En Lin <shiyn.lin@gmail.com>
2023-03-09 14:08:11 +0800

    lib/mthpc: Update mthpc library

--- include/ftc/task.h
+++ include/ftc/task.h
@@ -1,6 +1,8 @@
 #ifndef __FTC_TASK_H__
 #define __FTC_TASK_H__
 
+#include <mthpc/workqueue.h>
+
 #ifndef FILENAME_SIZE
 #define FILENAME_SIZE 80
 #endif
@@ -8,6 +10,7 @@
 struct task_struct {
     char name[FILENAME_SIZE + FILENAME_SIZE];
     unsigned long id;
+    struct mthpc_work work;
 };
 
 int execute_task(struct task_struct *task);
